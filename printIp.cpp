03f18507701aea00ac977fd113b05534c4b3fd74
AAGrigoriev <grigormaklaud@gmail.com>
2020-11-10 19:17:26 +0300

    Commit

--- printIp.cpp
+++ printIp.cpp
@@ -1,16 +1,51 @@
-#include "printIp.hpp"
+//#include "printIp.hpp"
+
+#include "iostream"
+
+class Primbase
+{
+public:
+    int value;
+    virtual Primbase *foo() // new slot in vtable
+    {
+        std::cout << "Prinbase\n";
+        return this;
+    }
+
+    virtual ~Primbase() {}
+};
+
+class Der : public Primbase
+{
+public:
+    // primary base
+    virtual Der *foo() override // replaces Primbase::foo() in vtable
+    {
+        std::cout << "Der\n";
+        static int value2 = 4;
+        return this;
+    }
+
+    virtual ~Der() {}
+};
 
 int main()
 {
-    otusPrint::IP_Pool<4> pool;
 
-    pool.read_from_stream(std::cin);
-    
-    pool.sort_reverse();
+Primbase* base = new Der;
 
-    pool.print();
 
-    std::cout << "\n";
+
+base->foo();
+    // otusPrint::IP_Pool<4> pool;
+
+    // pool.read_from_stream(std::cin);
+
+    // pool.sort_reverse();
+
+    //  pool.print();
+
+    // std::cout << "\n";
 
     //std::cout << pool.filter(1u,1u);
 
--- printIp.hpp
+++ printIp.hpp
@@ -10,132 +10,132 @@
 
 namespace otusPrint
 {
-template <std::size_t N>
-std::istream &operator>>(std::istream &os, std::array<uint32_t, N> &array);
+    template <std::size_t N>
+    std::istream &operator>>(std::istream &os, std::array<uint32_t, N> &array);
 
-template <std::size_t N>
-std::ostream &operator<<(std::ostream &os, const std::vector<std::array<uint32_t, N>> &vect_ip);
+    template <std::size_t N>
+    std::ostream &operator<<(std::ostream &os, const std::vector<std::array<uint32_t, N>> &vect_ip);
 
-template <std::size_t N>
-class IP_Pool
-{
-
-private:
-    std::vector<std::array<uint32_t, N>> vect_ip_octets;
-
-protected:
-public:
-    friend std::istream &operator>><>(std::istream &os, std::array<uint32_t, N> &array);
-
-    friend std::ostream &operator<<<>(std::ostream &os, const std::vector<std::array<uint32_t, N>> &vect_ip);
-
-    void read_from_stream(std::istream &stream)
+    template <std::size_t N>
+    class IP_Pool
     {
-        auto locale_prev = stream.imbue(std::locale(stream.getloc(), new printf_white_space));
 
-        std::array<uint32_t, N> addreses{};
+    private:
+        std::vector<std::array<uint32_t, N>> vect_ip_octets;
 
-        while (stream >> addreses)
+    protected:
+    public:
+        friend std::istream &operator>><>(std::istream &os, std::array<uint32_t, N> &array);
+
+        friend std::ostream &operator<<<>(std::ostream &os, const std::vector<std::array<uint32_t, N>> &vect_ip);
+
+        void read_from_stream(std::istream &stream)
         {
-            vect_ip_octets.push_back(std::move(addreses));
+            auto locale_prev = stream.imbue(std::locale(stream.getloc(), new printf_white_space));
 
-            stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::array<uint32_t, N> addreses{};
+
+            while (stream >> addreses)
+            {
+                vect_ip_octets.push_back(std::move(addreses));
+
+                stream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            stream.imbue(locale_prev);
         }
-        stream.imbue(locale_prev);
-    }
 
-    void sort_reverse()
-    {
-        std::sort(vect_ip_octets.begin(), vect_ip_octets.end(), [](std::array<uint32_t, N> &left, std::array<uint32_t, N> &right) {
-            for (std::size_t i = 0; i < N; ++i)
-            {
-                if (left[i] != right[i])
-                    return left[i] > right[i];
-            }
-            return false;
-        });
-    }
-
-    template <typename... Args>
-    std::vector<std::array<uint32_t, N>> filter(Args... args) const
-    {
-        static_assert(sizeof...(args) <= N);
-
-        std::array<uint32_t, sizeof...(args)> array_to_lamda = {args...};
-
-        auto lamda_find = [&array_to_lamda](const std::array<uint32_t, N> &array_in_vec) {
-            for (std::size_t i = 0; i < array_to_lamda.size(); ++i)
-            {
-                if (array_in_vec[i] != array_to_lamda[i])
-                    return false;
-            }
-            return true;
-        };
-
-        auto iterBegin = std::find_if(vect_ip_octets.begin(), vect_ip_octets.end(), lamda_find);
-
-        auto iterEnd = std::find_if_not(iterBegin, vect_ip_octets.end(), lamda_find);
-
-        return std::vector<std::array<uint32_t, N>>(iterBegin, iterEnd);
-    }
-
-    std::vector<std::array<uint32_t, N>> fileter_any(uint32_t arg) const
-    {
-        std::vector<std::array<uint32_t, N>> out;
-
-        auto lamda_find = [arg, &out](const std::array<uint32_t, N> &array) {
-            for (auto iterBeg = array.cbegin(); iterBeg != array.cend(); ++iterBeg)
-            {
-                if ((*iterBeg) == arg)
-                    out.push_back(array);
-            }
-        };
-
-        std::for_each(vect_ip_octets.begin(), vect_ip_octets.end(), lamda_find);
-
-        return out;
-    }
-
-    void print() const
-    {
-        for (std::size_t i = 0; i < vect_ip_octets.size(); ++i)
+        void sort_reverse()
         {
-            for (std::size_t j = 0; j < N; ++j)
-            {
-                if (j != 0)
-                    std::cout << ".";
-                std::cout << vect_ip_octets[i][j];
-            }
-            std::cout << std::endl;
+            std::sort(vect_ip_octets.begin(), vect_ip_octets.end(), [](std::array<uint32_t, N> &left, std::array<uint32_t, N> &right) {
+                for (std::size_t i = 0; i < N; ++i)
+                {
+                    if (left[i] != right[i])
+                        return left[i] > right[i];
+                }
+                return false;
+            });
         }
-    }
-};
 
-template <std::size_t N>
-std::ostream &operator<<(std::ostream &os, const std::vector<std::array<uint32_t, N>> &vect_ip)
-{
-    for (auto vec_begin = vect_ip.begin(); vec_begin != vect_ip.end(); ++vec_begin)
-    {
-        for (auto array_begin = (*vec_begin).begin(); array_begin != (*vec_begin).end(); ++array_begin)
+        template <typename... Args>
+        std::vector<std::array<uint32_t, N>> filter(Args... args) const
         {
-            os << (*array_begin);
-            if (array_begin < (*vec_begin).end() - 1)
-                os << ".";
+            static_assert(sizeof...(args) <= N);
+
+            std::array<uint32_t, sizeof...(args)> array_to_lamda = {args...};
+
+            auto lamda_find = [&array_to_lamda](const std::array<uint32_t, N> &array_in_vec) {
+                for (std::size_t i = 0; i < array_to_lamda.size(); ++i)
+                {
+                    if (array_in_vec[i] != array_to_lamda[i])
+                        return false;
+                }
+                return true;
+            };
+
+            auto iterBegin = std::find_if(vect_ip_octets.begin(), vect_ip_octets.end(), lamda_find);
+
+            auto iterEnd = std::find_if_not(iterBegin, vect_ip_octets.end(), lamda_find);
+
+            return std::vector<std::array<uint32_t, N>>(iterBegin, iterEnd);
         }
-        os << std::endl;
-    }
-    return os;
-}
 
-template <std::size_t N>
-std::istream &operator>>(std::istream &os, std::array<uint32_t, N> &array)
-{
-    for (uint32_t &octet : array)
+        std::vector<std::array<uint32_t, N>> fileter_any(uint32_t arg) const
+        {
+            std::vector<std::array<uint32_t, N>> out;
+
+            auto lamda_find = [arg, &out](const std::array<uint32_t, N> &array) {
+                for (auto iterBeg = array.cbegin(); iterBeg != array.cend(); ++iterBeg)
+                {
+                    if ((*iterBeg) == arg)
+                        out.push_back(array);
+                }
+            };
+
+            std::for_each(vect_ip_octets.begin(), vect_ip_octets.end(), lamda_find);
+
+            return out;
+        }
+
+        void print() const
+        {
+            for (std::size_t i = 0; i < vect_ip_octets.size(); ++i)
+            {
+                for (std::size_t j = 0; j < N; ++j)
+                {
+                    if (j != 0)
+                        std::cout << ".";
+                    std::cout << vect_ip_octets[i][j];
+                }
+                std::cout << std::endl;
+            }
+        }
+    };
+
+    template <std::size_t N>
+    std::ostream &operator<<(std::ostream &os, const std::vector<std::array<uint32_t, N>> &vect_ip)
     {
-        if (!(os >> octet))
-            return os;
+        for (auto vec_begin = vect_ip.begin(); vec_begin != vect_ip.end(); ++vec_begin)
+        {
+            for (auto array_begin = (*vec_begin).begin(); array_begin != (*vec_begin).end(); ++array_begin)
+            {
+                os << (*array_begin);
+                if (array_begin < (*vec_begin).end() - 1)
+                    os << ".";
+            }
+            os << std::endl;
+        }
+        return os;
     }
-    return os;
-}
 
-}
+    template <std::size_t N>
+    std::istream &operator>>(std::istream &os, std::array<uint32_t, N> &array)
+    {
+        for (uint32_t &octet : array)
+        {
+            if (!(os >> octet))
+                return os;
+        }
+        return os;
+    }
+
+} // namespace otusPrint
